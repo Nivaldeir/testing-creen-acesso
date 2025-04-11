b8f4dd8e248f8b8d5be299274816602b95d8040a
Nivaldeir Silva <nivaldeir-silva@hotmail.com>
2025-04-11 11:42:12 -0300

    fix: alter logic for query

--- src/core/app/usecases/find-boleto.ts
+++ src/core/app/usecases/find-boleto.ts
@@ -7,6 +7,8 @@ type Input = {
     nome?: string;
     id_lote?: string | number;
     relatorio?: string | number;
+    valor_inicial?: string | number
+    valor_final?: string | number
   };
 };
 
@@ -14,16 +16,10 @@ export class FindBoleto {
   constructor(private readonly repositoryBoleto: RepostiryBoleto) { }
 
   async execute(input: Input) {
-    const { nome, id_lote, relatorio } = input.query;
-
-    const query = {
-      ...(nome && { nome_sacado: nome }),
-      ...(id_lote && { id_lote: Number(id_lote) }),
-    };
-
+    const query = this.returnQueryToPrisma(input)
     const boletos = await this.repositoryBoleto.find(query);
 
-    const isRelatorio = String(relatorio) === "1";
+    const isRelatorio = String(input.query.relatorio) === "1";
     if (isRelatorio) {
       if (boletos.length === 0) return { base64: null };
       const base64 = await this.generatePdf(boletos);
@@ -62,5 +58,28 @@ export class FindBoleto {
 
     const pdfBytes = await pdfDoc.save();
     return Buffer.from(pdfBytes).toString("base64");
+  }
+
+  private returnQueryToPrisma(params: Input) {
+    const { nome, id_lote, valor_final, valor_inicial } = params.query;
+
+    const query: any = {};
+
+    if (nome) {
+      query.nome_sacado = {
+        contains: nome,
+      };
+    }
+
+    if (id_lote) {
+      query.id_lote = Number(id_lote);
+    }
+
+    if (valor_inicial || valor_final) {
+      query.valor = {};
+      if (valor_inicial) query.valor.gte = Number(valor_inicial);
+      if (valor_final) query.valor.lte = Number(valor_final);
+    }
+    return query
   }
 }
